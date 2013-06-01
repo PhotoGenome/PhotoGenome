8f7c86df75e5bf39a41799bb6e685d6f4e744b1f
hemant99911 <hemant99911@gmail.com>
2013-06-01 16:16:05 -0400

    embed class

---
+++ PhotoGenome/src/main/java/edu/cmu/photogenome/actions/embedinformation/EmbedInformationController.java
@@ -0,0 +1,130 @@
+package edu.cmu.photogenome.actions.embedinformation;
+
+import java.util.List;
+
+import edu.cmu.photogenome.domain.*;
+
+public class EmbedInformationController {
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean addPhotoComments(Photo currentPhoto, List<PhotoComment> lstPhotoComments){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean addPhotoCategories(Photo currentPhoto, List<PhotoCategory> lstPhotoCategories){
+		
+	}
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean addPhotoRegions(Photo currentPhoto, List<PhotoRegion> lstPhotoRegions){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean addRegionComments(PhotoRegion currentPhotoRegion, List<RegionComment> lstRegionComments){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean addRegionCategories(PhotoRegion currentPhotoRegion, List<RegionCategory> lstRegionCategories){
+		
+	}
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean addRegionCoordinates(PhotoRegion currentPhotoRegion, List<RegionCoor> lstRegionCoordinates){
+		
+	}
+	
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean editPhotoComments(Photo currentPhoto, PhotoComment currentPhotoComments){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean editPhotoCategories(Photo currentPhoto,PhotoCategory currentPhotoCategories){
+		
+	}
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean editPhotoRegions(Photo currentPhoto, PhotoRegion currentPhotoRegions){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean editRegionComments(PhotoRegion currentPhotoRegion, RegionComment currentRegionComments){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean editRegionCategories(PhotoRegion currentPhotoRegion, RegionCategory currentRegionCategories){
+		
+	}
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean editRegionCoordinates(PhotoRegion currentPhotoRegion, List<RegionCoor> lstRegionCoordinates){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean deletePhotoComments(Photo currentPhoto, List<PhotoComment> lstPhotoComments){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean deletePhotoCategories(Photo currentPhoto, List<PhotoCategory> lstPhotoCategories){
+		
+	}
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean deletePhotoRegions(Photo currentPhoto, List<PhotoRegion> lstPhotoRegions){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean deleteRegionComments(PhotoRegion currentPhotoRegion, List<RegionComment> lstRegionComments){
+		
+	}
+
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean deleteRegionCategories(PhotoRegion currentPhotoRegion, List<RegionCategory> lstRegionCategories){
+		
+	}
+	/* takes a user object and verifies its login details it into the system
+	 * return true if correct*/
+	
+	public boolean deleteRegionCoordinates(PhotoRegion currentPhotoRegion, List<RegionCoor> lstRegionCoordinates){
+		
+	}
+
+
+}
