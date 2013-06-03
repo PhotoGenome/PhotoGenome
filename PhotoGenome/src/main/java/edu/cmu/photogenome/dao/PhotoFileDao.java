9e6b2623799593d0fb42a3d88af1ad4d810fcbcd
tgbadorrek <tgbadorrek@gmail.com>
2013-06-02 21:04:01 -0400

    added photo file DAO classes -PhotoFile.java -PhotoFileDao.java -PhotoFileDaoImpl.java

--- PhotoGenome/src/main/java/edu/cmu/photogenome/dao/PhotoFileDao.java
+++ PhotoGenome/src/main/java/edu/cmu/photogenome/dao/PhotoFileDao.java
@@ -1,5 +1,13 @@
 package edu.cmu.photogenome.dao;
 
+import edu.cmu.photogenome.domain.PhotoFile;
+
 public interface PhotoFileDao {
+	
+	public boolean delete(PhotoFile photo);
+	
+	public PhotoFile retrievePhoto(String location);
+	
+	public boolean save(PhotoFile photo);
 	
 }
