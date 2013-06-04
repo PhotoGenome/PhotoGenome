782a12f0e1372dd25d3880bb27099fc34dfa42af
tgbadorrek <tgbadorrek@gmail.com>
2013-06-04 01:22:17 -0400

    updated embedInfoAction

--- PhotoGenome/src/main/java/edu/cmu/photogenome/actions/embedinformation/EmbedInformationAction.java
+++ PhotoGenome/src/main/java/edu/cmu/photogenome/actions/embedinformation/EmbedInformationAction.java
@@ -15,8 +15,6 @@ public class EmbedInformationAction extends ActionSupport {
 	private RegionCategoryDao regionCategoryDao = new RegionCategoryDaoImpl();
 	private RegionCoordinateDao regionCoordinateDao = new RegionCoordinateDaoImpl();
 	
-	private PhotoCategoryDao photoCategoryDao = new PhotoCategoryDaoImpl();
-	
 	/* takes a user object and verifies its login details it into the system
 	 * return true if correct*/
 	
@@ -27,44 +25,32 @@ public class EmbedInformationAction extends ActionSupport {
 	/* takes a user object and verifies the login details it into the system
 	 * return true if correct*/
 	
-	public boolean addPhotoCategories(int photoId, int userId, List<String> categoryDetails,
-			String photoCategoryText){
-		PhotoCategory category = new PhotoCategory(photoId, userId, new Date());
-		
-		for (String details : categoryDetails){
-			String[] categoryNameandText = details.split(":");
-			category.setPhotoCategoryName([0]);
-			category.setPhotoCategoryText(photoCategoryText);
-		}
-		
-		
-		
-		
-		if(!photoCategoryDao.save(category))
-			return false;
-		return true;
-	}
-	
-	/* takes a user object and verifies the login details it into the system
-	 * return true if correct*/
-	
 	public boolean addPhotoCategories(Photo currentPhoto, List<PhotoCategory> listPhotoCategories){
 		return false;
 	}
 	
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
+	/**
+	 * Save a region marked on a photo
+	 * 
+	 * @param photoId
+	 * @param userId
+	 * @param shapeId
+	 * @param regionX
+	 * @param regionY
+	 * @param height
+	 * @param width
+	 * @return true if the region is saved, else false
+	 */
 	public boolean addPhotoRegion(int photoId, int userId, int shapeId, int regionX, int regionY, 
 			int height, int width) {
-		PhotoRegion region = new PhotoRegion(photoId, userId, shapeId, new Date());
-		if(!photoRegionDao.save(region))
+		PhotoRegion region = null;
+		if((region = addPhotoRegion(photoId, userId, shapeId)) == null) // try to save the region
 			return false;
 		else {
-			RegionCoordinate coordinate = new RegionCoordinate(region.getRegionId(), photoId, userId, 
-				regionX, regionY, height, width, new Date());
-			if(!regionCoordinateDao.save(coordinate)) {
-				photoRegionDao.delete(region);
+			RegionCoordinate coordinate = null;
+			if((coordinate = addRegionCoordinate(region.getRegionId(), photoId, userId, regionX, regionY, 
+					height, width)) == null) { // try to save the coordinates
+				photoRegionDao.delete(region); // delete region if coordinates failed to save
 				return false;
 			}
 		}
@@ -72,62 +58,70 @@ public class EmbedInformationAction extends ActionSupport {
 		return true;
 	}
 	
-	public boolean addPhotoRegions(Photo currentPhoto, List<PhotoRegion> listPhotoRegions){
-		for(PhotoRegion region : listPhotoRegions) {
-			if(!photoRegionDao.save(region))
-				return false;
-		}
-		
-		return true;
-	}
-	
-	public boolean addPhotoRegions(int photoId, int userId, int shapeId){
+	/**
+	 * Private helper method to save a photo region
+	 * 
+	 * @param photoId
+	 * @param userId
+	 * @param shapeId
+	 * @return the saved photo region, or null if failed to save
+	 */
+	private PhotoRegion addPhotoRegion(int photoId, int userId, int shapeId) {
 		PhotoRegion region = new PhotoRegion(photoId, userId, shapeId, new Date());
-		return photoRegionDao.save(region);
+		
+		if(photoRegionDao.save(region))
+			return region;
+		else
+			return null;
+	}
+
+	/**
+	 * Save a comment on a region
+	 * 
+	 * @param photoId
+	 * @param userId
+	 * @param regionId
+	 * @param regionCommentText
+	 * @return true if the comment was saved, else false
+	 */
+	public boolean addRegionComment(int photoId, int userId, int regionId, String regionCommentText) {
+		RegionComment comment = new RegionComment(photoId, regionId, userId, new Date());
+		comment.setRegionCommentText(regionCommentText);
+		
+		if(regionCommentDao.save(comment))
+			return true;
+		else
+			return false;
 	}
 
 	/* takes a user object and verifies its login details it into the system
 	 * return true if correct*/
 	
-	public boolean addRegionComments(PhotoRegion currentPhotoRegion, List<RegionComment> listRegionComments){
-		if(photoRegionDao.findById(currentPhotoRegion.getRegionId()) == null)
-			return false;
-		
-		for(RegionComment regionComment : listRegionComments) {
-			if(!regionCommentDao.save(regionComment))
-				return false;
-		}
-		
-		return true;
+	private boolean addRegionCategories(PhotoRegion currentPhotoRegion, List<String> listRegionCategories){
+		return false;
 	}
-
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
 	
-	public boolean addRegionCategories(PhotoRegion currentPhotoRegion, List<RegionCategory> listRegionCategories){
-		if(photoRegionDao.findById(currentPhotoRegion.getRegionId()) == null)
-			return false;
+	/**
+	 * Private helper method to save a set of region coordinates
+	 * 
+	 * @param regionId
+	 * @param photoId
+	 * @param userId
+	 * @param regionX
+	 * @param regionY
+	 * @param height
+	 * @param width
+	 * @return the saved region coordinates, or null if failed to save
+	 */
+	private RegionCoordinate addRegionCoordinate(int regionId, int photoId, int userId, int regionX, int regionY, 
+			int height, int width){
+		RegionCoordinate coordinate = new RegionCoordinate(regionId, photoId, userId, regionX, regionY, 
+				height, width, new Date());
 		
-		for(RegionCategory regionCategory : listRegionCategories) {
-			if(!regionCategoryDao.save(regionCategory))
-				return false;
-		}
-		
-		return true;
-	}
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean addRegionCoordinates(PhotoRegion currentPhotoRegion, List<RegionCoordinate> listRegionCoordinates){
-		if(photoRegionDao.findById(currentPhotoRegion.getRegionId()) == null)
-			return false;
-		
-		for(RegionCoordinate regionCoordinate : listRegionCoordinates) {
-			if(!regionCoordinateDao.save(regionCoordinate))
-				return false;
-		}
-		
-		return true;
+		if(!regionCoordinateDao.save(coordinate))
+				return coordinate;
+		else
+			return null;
 	}
 	
 	/* takes a user object and verifies its login details it into the system
@@ -140,73 +134,40 @@ public class EmbedInformationAction extends ActionSupport {
 	/* takes a user object and verifies its login details it into the system
 	 * return true if correct*/
 	
-	public boolean editPhotoCategories(int photoId, int userId, String photoCategoryName,
-			String photoCategoryText){
-		
-		PhotoCategory category = new PhotoCategory();
-		category.setPhotoId(photoId);
-		category.setUserId(userId);
-		category.setPhotoCategoryName(photoCategoryName);
-		category.setPhotoCategoryText(photoCategoryText);
-		
-		if(!photoCategoryDao.update(category))
-			return false;
-		return true;
-		
-	}
-	
-	
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
 	public boolean editPhotoCategories(Photo currentPhoto,PhotoCategory currentPhotoCategories){
 		return false;
 	}
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean editPhotoRegions(Photo currentPhoto, PhotoRegion currentPhotoRegions){
-		return false;
-	}
 
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean editRegionComments(PhotoRegion currentPhotoRegion, RegionComment currentRegionComments){
-		return false;
-	}
-
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean editRegionCategories(int regionId, int photoId, int userId,
-			String categoryName, String regionCategoryText,
-			Boolean regionCategoryIsdeleted){
-		
-		RegionCategory category = new RegionCategory();
-		category.setRegionId(regionId);
-		category.setPhotoId(photoId);
-		category.setCategoryName(categoryName);
-		category.setRegionCategoryText(regionCategoryText);
-		
-		if(!regionCategoryDao.update(category))
+	/**
+	 * Edit the text of a region comment
+	 * 
+	 * @param comment
+	 * @return true if the comment is updated, else false
+	 */
+	public boolean editRegionComments(RegionComment comment){
+		if(regionCommentDao.update(comment))
+			return true;
+		else
 			return false;
-		
-		return true;
 	}
 
-	
 	/* takes a user object and verifies its login details it into the system
 	 * return true if correct*/
 	
 	public boolean editRegionCategories(PhotoRegion currentPhotoRegion, RegionCategory currentRegionCategories){
 		return false;
 	}
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
 	
-	public boolean editRegionCoordinates(PhotoRegion currentPhotoRegion, List<RegionCoordinate> listRegionCoordinates){
-		return false;
+	/**
+	 * Edit a region coordinate
+	 * @param coordinate
+	 * @return true if the coordinate is updated, else false
+	 */
+	public boolean editRegionCoordinate(RegionCoordinate coordinate){
+		if(regionCoordinateDao.update(coordinate))
+			return true;
+		else
+			return false;
 	}
 
 	/* takes a user object and verifies its login details it into the system
@@ -216,71 +177,63 @@ public class EmbedInformationAction extends ActionSupport {
 		return false;
 	}
 
-	
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean deletePhotoCategories(int photoId, int photoCategoryId){
-		
-		PhotoCategory category = new PhotoCategory();
-		
-		category.setPhotoId(photoId);
-		category.setPhotoCategoryId(photoCategoryId);
-		
-		if(!photoCategoryDao.delete(category))
-			return false;
-		
-		return true;
-	}
-	
 	/* takes a user object and verifies its login details it into the system
 	 * return true if correct*/
 	
 	public boolean deletePhotoCategories(Photo currentPhoto, List<PhotoCategory> listPhotoCategories){
 		return false;
 	}
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
 	
-	public boolean deletePhotoRegions(Photo currentPhoto, List<PhotoRegion> listPhotoRegions){
-		return false;
-	}
-
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean deleteRegionComments(PhotoRegion currentPhotoRegion, List<RegionComment> listRegionComments){
-		return false;
-	}
-
-	
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
-	
-	public boolean deleteRegionCategories(int regionId, int photoId, int regionCategoryId){
-		
-		RegionCategory category = new RegionCategory();
-		category.setRegionCategoryId(regionCategoryId);
-		category.setRegionId(regionId);
-		category.setPhotoId(photoId);
-		
-		if(!regionCategoryDao.delete(category))
-			return false;
+	/**
+	 * Delete a photo region
+	 * @param regionId
+	 * @return true if the region no longer exists, else false
+	 */
+	public boolean deletePhotoRegion(int regionId){
+		PhotoRegion region;
+		if((region = photoRegionDao.findById(regionId)) != null) {
+			if(!photoRegionDao.delete(region))
+				return false;
+		}
 		
 		return true;
 	}
-	
+
+	/**
+	 * Delete a region comment
+	 * @param commentId
+	 * @return true if the region comment no longer exists, else false
+	 */
+	public boolean deleteRegionComment(int commentId){
+		RegionComment comment;
+		if((comment = regionCommentDao.findById(commentId)) != null) {
+			if(!regionCommentDao.delete(comment))
+				return false;
+		}
+		
+		return true;
+	}
+
 	/* takes a user object and verifies its login details it into the system
 	 * return true if correct*/
 	
 	public boolean deleteRegionCategories(PhotoRegion currentPhotoRegion, List<RegionCategory> listRegionCategories){
 		return false;
 	}
-	/* takes a user object and verifies its login details it into the system
-	 * return true if correct*/
 	
-	public boolean deleteRegionCoordinates(PhotoRegion currentPhotoRegion, List<RegionCoordinate> listRegionCoordinates){
-		return false;
+	/**
+	 * Delete a region coordinate
+	 * @param coordinateId
+	 * @return true if the region coordinate no longer exists, else false
+	 */
+	public boolean deleteRegionCoordinates(int coordinateId) {
+		RegionCoordinate coordinate;
+		if((coordinate = regionCoordinateDao.findById(coordinateId)) != null) {
+			if(!regionCoordinateDao.delete(coordinate))
+				return false;
+		}
+		
+		return true;		
 	}
 
 
