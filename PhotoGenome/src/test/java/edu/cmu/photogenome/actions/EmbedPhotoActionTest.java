c7785ef8f9fa325aeaadc9535b674edcefddbf5c
hemant99911 <hemant99911@gmail.com>
2013-06-19 15:32:01 -0400

    updated EmbedPhotoActionTest, added more actions in struts.xml

--- PhotoGenome/src/test/java/edu/cmu/photogenome/actions/EmbedPhotoActionTest.java
+++ PhotoGenome/src/test/java/edu/cmu/photogenome/actions/EmbedPhotoActionTest.java
@@ -1,45 +1,124 @@
 package edu.cmu.photogenome.actions;
 
-import static org.junit.Assert.*;
+import java.util.Map;
 
 import org.apache.struts2.StrutsTestCase;
 import org.junit.Test;
 
+import com.opensymphony.xwork2.ActionProxy;
+
+import edu.cmu.photogenome.domain.PhotoCategory;
+import edu.cmu.photogenome.domain.PhotoComment;
+
 public class EmbedPhotoActionTest extends StrutsTestCase {
 
 	@Test
-	public void testAddPhotoComments() {
-		fail("Not yet implemented");
+	public void testAddPhotoComments() throws Exception{
+		request.setParameter("photoId", "1");
+		request.setParameter("userId", "1000");
+		request.setParameter("photoCommentText", "test comment");
+
+		ActionProxy proxy = getActionProxy("/addPhotoComments");
+		EmbedPhotoAction action = (EmbedPhotoAction) proxy.getAction();
+		String result = proxy.execute();
+		Map<String, Object> jsonData = action.getJsonAddPhotoComments();
+		assertNotNull(jsonData);
+		PhotoComment photoComment = (PhotoComment) jsonData.get("items");
+		assertEquals(1, photoComment.getPhotoId());
+		assertEquals("success", result);
 	}
 
 	@Test
-	public void testAddPhotoCategories() {
-		fail("Not yet implemented");
+	public void testAddPhotoCommentsNull() throws Exception {
+		request.setParameter("photoId", "-1");
+
+		ActionProxy proxy = getActionProxy("/addPhotoComments");
+		//EmbedPhotoAction action = (EmbedPhotoAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("error", result);
 	}
 
 	@Test
-	public void testEditPhotoComments() {
-		fail("Not yet implemented");
+	public void testAddPhotoCategories() throws Exception {		
+		request.setParameter("photoId", "1");
+		request.setParameter("userId", "1000");
+		request.setParameter("photoCategoryName", "test category name");
+		request.setParameter("photoCategoryText", "test category text");		
+
+		ActionProxy proxy = getActionProxy("/addPhotoCategories");
+		EmbedPhotoAction action = (EmbedPhotoAction) proxy.getAction();
+		String result = proxy.execute();
+		Map<String, Object> jsonData = action.getJsonAddPhotoCategories();
+		assertNotNull(jsonData);
+		PhotoCategory photoCategory = (PhotoCategory) jsonData.get("items");
+		assertEquals(1, photoCategory.getPhotoId());
+		assertEquals("success", result);
 	}
 
 	@Test
-	public void testEditPhotoCategories() {
-		fail("Not yet implemented");
+	public void testAddPhotoCategoriesNull() throws Exception {
+		request.setParameter("photoId", "-1");
+
+		ActionProxy proxy = getActionProxy("/addPhotoCategories");
+		//EmbedPhotoAction action = (EmbedPhotoAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("error", result);
 	}
 
 	@Test
-	public void testEditRegionCategories() {
-		fail("Not yet implemented");
+	public void testEditPhotoComments() throws Exception {
+
+		request.setParameter("photoCommentId", "1");
+		request.setParameter("photoCommentText", "test action comment");
+
+		ActionProxy proxy = getActionProxy("/editPhotoComments");
+		//EmbedRegionAction action = (EmbedRegionAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("success", result);
 	}
 
 	@Test
-	public void testDeletePhotoComments() {
-		fail("Not yet implemented");
+	public void testEditPhotoCategories() throws Exception {
+		request.setParameter("photoId", "1");
+		request.setParameter("photoCategoryName", "test category name");
+		request.setParameter("photoCategoryText", "test category text");
+
+		ActionProxy proxy = getActionProxy("/editPhotoCategories");
+		//EmbedRegionAction action = (EmbedRegionAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("success", result);
 	}
 
 	@Test
-	public void testDeletePhotoCategories() {
-		fail("Not yet implemented");
+	public void testEditRegionCategories() throws Exception {
+		request.setParameter("regionCategoryId", "1");
+		request.setParameter("categoryName", "test category name");
+		request.setParameter("regionCategoryText", "test category text");
+
+		ActionProxy proxy = getActionProxy("/editRegionCategories");
+		//EmbedRegionAction action = (EmbedRegionAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("success", result);
+	}
+
+	@Test
+	public void testDeletePhotoComments() throws Exception {
+		request.setParameter("photoCommentId", "1");
+
+		ActionProxy proxy = getActionProxy("/deletePhotoComments");
+		//EmbedRegionAction action = (EmbedRegionAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("success", result);
+	}
+
+	@Test
+	public void testDeletePhotoCategories() throws Exception {
+		request.setParameter("photoCategoryId", "1");
+
+		ActionProxy proxy = getActionProxy("/deletePhotoCategories");
+		//EmbedRegionAction action = (EmbedRegionAction) proxy.getAction();
+		String result = proxy.execute();
+		assertEquals("success", result);
 	}
 
 }
