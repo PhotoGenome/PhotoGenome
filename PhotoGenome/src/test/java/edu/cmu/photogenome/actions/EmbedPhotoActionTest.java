d4c8cf530a4a1631143b20cf10254d3abdc56bdf
hemant99911 <hemant99911@gmail.com>
2013-06-19 10:57:49 -0400

    Added EmbedPhotoActionTest

---
+++ PhotoGenome/src/test/java/edu/cmu/photogenome/actions/EmbedPhotoActionTest.java
@@ -0,0 +1,45 @@
+package edu.cmu.photogenome.actions;
+
+import static org.junit.Assert.*;
+
+import org.apache.struts2.StrutsTestCase;
+import org.junit.Test;
+
+public class EmbedPhotoActionTest extends StrutsTestCase {
+
+	@Test
+	public void testAddPhotoComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPhotoCategories() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditPhotoComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditPhotoCategories() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditRegionCategories() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePhotoComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePhotoCategories() {
+		fail("Not yet implemented");
+	}
+
+}
