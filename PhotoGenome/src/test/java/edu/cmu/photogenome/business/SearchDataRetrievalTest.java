b49504485abcac0b4526e1659305b2ac023ae4b4
tgbadorrek <tgbadorrek@gmail.com>
2013-07-18 16:52:20 -0400

    added entity classes for ImportedMetadata and PhotoMetadata

--- PhotoGenome/src/test/java/edu/cmu/photogenome/business/SearchDataRetrievalTest.java
+++ PhotoGenome/src/test/java/edu/cmu/photogenome/business/SearchDataRetrievalTest.java
@@ -11,15 +11,69 @@ import edu.cmu.photogenome.util.HibernateDbUnitTestCase;
 public class SearchDataRetrievalTest extends HibernateDbUnitTestCase {
 
 	@Test
+	public void testSearchAssociatedPhotos() {
+		int photoId = 1;
+		int maxMatches = 3;
+		
+		SearchDataRetrieval search = new SearchDataRetrieval(session);
+		List<Photo> results = search.searchAssociatedPhotos(photoId, maxMatches);
+		
+		assertNotNull(results);
+	}
+
+	@Test
+	public void testSearchAssociatedPhotosEmptyResults() {
+		int photoId = 1;
+		int maxMatches = 3;
+		
+		SearchDataRetrieval search = new SearchDataRetrieval(session);
+		List<Photo> results = search.searchAssociatedPhotos(photoId, maxMatches);
+		
+		assertNotNull(results);
+		assertEquals(0, results.size());
+	}
+	
+	@Test
 	public void testSearchFilteredAssociatedPhotos() {
 		int photoId = 1;
 		int maxMatches = 3;
 		List<String> categories = new ArrayList<String>();
-		
 		SearchDataRetrieval search = new SearchDataRetrieval(session);
-		List<Photo> results = search.searchFilteredAssociatedPhotos(photoId, categories, maxMatches);
+		List<Photo> results = null;
 		
+		categories.add("buildingWeanHall");
+		results = search.searchFilteredAssociatedPhotos(photoId, categories, maxMatches);
 		assertNotNull(results);
+		assertEquals(1, results.size());
+		System.out.println("size = " + results.size());
+		for(Photo p : results)
+			System.out.println(p.getPhotoId());
+		
+		categories.clear();
+		categories.add("buildingHamerschlagHall");
+		results = search.searchFilteredAssociatedPhotos(photoId, categories, maxMatches);
+		assertNotNull(results);
+		assertEquals(1, results.size());
+		System.out.println("size = " + results.size());
+		for(Photo p : results)
+			System.out.println(p.getPhotoId());
+		
+		categories.clear();
+		categories.add("personTyler");
+		results = search.searchFilteredAssociatedPhotos(photoId, categories, maxMatches);
+		assertNotNull(results);
+		assertEquals(2, results.size());
+		System.out.println("size = " + results.size());
+		for(Photo p : results)
+			System.out.println(p.getPhotoId());
+		
+		categories.add("buildingHamerschlagHall");
+		results = search.searchFilteredAssociatedPhotos(photoId, categories, maxMatches);
+		assertNotNull(results);
+		assertEquals(2, results.size());
+		System.out.println("size = " + results.size());
+		for(Photo p : results)
+			System.out.println(p.getPhotoId());
 	}
 
 	@Test
